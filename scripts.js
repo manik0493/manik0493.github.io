f64bb272fe1f4729a76a122d2d16e4cf13fafc89
Manik Narang <manik.narang@yahoo.com>
2020-01-22 14:02:57 -0600

    animation changes
    animation changes

--- scripts.js
+++ scripts.js
@@ -16,7 +16,7 @@ jQuery(function($) {
       // Check all animatables and animate them if necessary
           $animatables.each(function(i) {
          var $animatable = $(this);
-              if (($animatable.offset().top + $animatable.height() - 20) < offset) {
+              if (($animatable.offset().top + $animatable.height() - 5) < offset) {
           $animatable.removeClass('animatable').addClass('animated');
               }
       });
